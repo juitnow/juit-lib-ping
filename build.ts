1f52ae6bd689c01b9bf5eab763a1646b8ebca822
Pier Fumagalli <pier@usrz.com>
2025-08-25 18:57:15 +0200

    Allow ping on Linux

--- build.ts
+++ build.ts
@@ -1,3 +1,19 @@
-import { tasks } from '@plugjs/build'
+import os from 'node:os'
 
-export default tasks()
+import { banner, exec, hookBefore, log, plugjs, tasks } from '@plugjs/build'
+
+const build = plugjs({
+  ...tasks(),
+
+  async _enable_ping(): Promise<void> {
+    if (os.platform() === 'linux') {
+      banner('Enabling ping for non-root users')
+      const uid = process.getuid!()
+      await exec('sudo', '-n', 'sysctl', '-w', `net.ipv4.ping_group_range=${uid} ${uid}`).catch((error) => log.error(error))
+      await exec('sudo', '-n', 'sysctl', '-w', `net.ipv6.ping_group_range=${uid} ${uid}`).catch((error) => log.error(error))
+    }
+  },
+})
+
+hookBefore(build, 'test', [ '_enable_ping' ])
+export default build
